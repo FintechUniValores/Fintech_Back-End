3ca9834fa4ac7abf4d08ae79017f7c7cdde1f730
valmir.formigari@gmail.com <102730777+ValmirFormigari@users.noreply.github.com>
2025-05-24 15:30:53 -0300

    fintech

---
+++ src/main/java/br/com/fintech/valoresareceber/BackendValoresAReceberApplication.java
@@ -0,0 +1,13 @@
+package br.com.fintech.valoresareceber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendValoresAReceberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendValoresAReceberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fintech/valoresareceber/BackendValoresAReceberApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fintech.valoresareceber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendValoresAReceberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
